dd5c9c3546d706baa30bc1b23359ff45a2d2cd5c
quin <g.joaquin000@gmail.com>
2023-05-29 15:02:10 -0700

    more changes to the landing page, fixed the presentation whenever the screen resizees

--- frontend/src/components/layout/body/landingPage1.js
+++ frontend/src/components/layout/body/landingPage1.js
@@ -1,11 +1,45 @@
 import React from "react";
 import classes from './landingPage1.module.css';
 
-
 const LandingPage1 = () => {
     return (
         <div className={classes.main}>
-            Landing Page 1
+            <div className={classes.container1}>
+                <div className={classes.left}>
+                </div>
+                <div className={classes.right}>
+                    <div className={classes.title}>
+                        Learn
+                    </div>
+                    <div className={classes.content}>
+                        <div className={classes.line}>
+                            <div className={classes.sub_title}>
+                                SELF-GUIDED
+                            </div>
+                            <div className={classes.sub_content}>
+                                Use TellusEd whenever you get stuck learning something, need direction, or want to deep-dive on a subject.
+                            </div>
+                        </div>
+                        <div className={classes.line}>
+                            <div className={classes.sub_title}>
+                            </div>
+                            <div className={classes.sub_content}>
+                            </div>
+                        </div>
+                        <div className={classes.line}>
+                            <div className={classes.sub_title}>
+                            </div>
+                            <div className={classes.sub_content}>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={classes.container2}>
+                    <div className={classes.toggle}>
+                        Toggle Button
+                    </div>
+            </div>
         </div>
     );
 }
