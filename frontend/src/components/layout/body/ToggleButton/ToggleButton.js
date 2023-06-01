6d3fd4e546358fb4a1c20cfcced8ba72956cf519
quin <g.joaquin000@gmail.com>
2023-06-01 02:18:45 -0700

    fixed the 2nd part of the landing page, specifically the ui toggle, design and coloring, next is to have the toggle create an action in react to change the content

--- frontend/src/components/layout/body/ToggleButton/ToggleButton.js
+++ frontend/src/components/layout/body/ToggleButton/ToggleButton.js
@@ -1,13 +1,21 @@
-import React from "react";
-
+import React,{ useState } from "react";
 import classes from './ToggleButton.module.css';
 
 const ToggleButton = () =>{
+    const [isValid, setIsValid] = useState(true);
+
+    const handleToggle = () => {
+        setIsValid(!isValid);
+    };
 
     return(
-        <div className={classes.toggleArea}>
+        <div className={classes.toggleSection}>
             <label className={classes.switch}>
-                <input type="checkbox"/>
+                <div className={classes.sliderText}>
+                    <div className={`${classes['left']} ${!isValid && classes.invalid}`}>Learn</div>
+                    <div className={`${classes['right']} ${!isValid && classes.invalid}`}>Contribute</div>
+                </div>
+                <input type="checkbox" checked={isValid} onChange={handleToggle}/>
                 <span className={classes.slider}></span>
             </label>
         </div>
