7815df3cc248896663c6ee6cbe145bb267143548
quin <g.joaquin000@gmail.com>
2023-07-29 22:01:07 -0700

    added the search input to the navbar menu, also added the css

--- frontend/src/components/layout/header/navbarMenu.js
+++ frontend/src/components/layout/header/navbarMenu.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import { useDispatch } from "react-redux";
-import { userActions } from '../../../reducers/auth';
+import SearchBar from './searchBar';
 import classes from './NavbarMenu.module.css';
 
 const NavbarMenu = () => {
-
-    const dispatch = useDispatch();
-    const exitAction = () => {
-        dispatch(userActions.navBarAsGuestOpenClose());
-    }
 
     return (
         <div className={classes.blurredBackgroundContainer}>
             <menu className={classes.menuContainer}>
                 <div className={classes.exitButtonPlaceholder}></div>
+
                 <div className={classes.menuContentContainer}>
-                    <ol className={classes.orderedList}>
+                    <ul className={classes.orderedList}>
                         <li>
-                            searchBar
+                            <SearchBar />
                         </li>
                         <li>Platform</li>
                         <li>Explore</li>
@@ -25,7 +20,7 @@ const NavbarMenu = () => {
                         <li>English</li>
                         <li>Log in</li>
                         <li>GetStartedButton</li>
-                    </ol>
+                    </ul>
                 </div>
             </menu>
         </div>
