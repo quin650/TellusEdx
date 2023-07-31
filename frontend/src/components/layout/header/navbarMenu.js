12c1d3f064575f512d26bc685a5b2d9cbb87c5e7
quin <g.joaquin000@gmail.com>
2023-07-30 18:25:19 -0700

    made some changes to the guest navbar menu

--- frontend/src/components/layout/header/navbarMenu.js
+++ frontend/src/components/layout/header/navbarMenu.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import SearchBar from './searchBar';
+import GetStartedButton from '../getStarted/button/GetStartedButton';
 import classes from './NavbarMenu.module.css';
 
 const NavbarMenu = () => {
 
     return (
         <div className={classes.blurredBackgroundContainer}>
-            <menu className={classes.menuContainer}>
+            <menu className={classes.menuContainer}>                        {/* 420px */}
                 <div className={classes.exitButtonPlaceholder}></div>
-
-                <div className={classes.menuContentContainer}>
+                                                    
+                <div className={classes.menuContentContainer}>              {/* 350px left and right 30px margin  */}
                     <ul className={classes.orderedList}>
-                        <li>
-                            <SearchBar />
-                        </li>
-                        <li>Platform</li>
+                        <div className={classes.searchContainer}>            {/* 350px */}
+                            <SearchBar />                                   {/* 320rem */}
+                        </div>
+                        <li>Platform</li>                                   {/* 320rem */}
                         <li>Explore</li>
+                        <div className={classes.break}></div>
                         <li>Theme</li>
                         <li>English</li>
+                        <div className={classes.break}></div>
                         <li>Log in</li>
-                        <li>GetStartedButton</li>
+                        <li>
+                            <GetStartedButton />
+                        </li>
                     </ul>
                 </div>
             </menu>
