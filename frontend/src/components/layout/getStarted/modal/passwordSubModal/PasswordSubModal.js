2593838d2ea4fe2b3677bc4b800cabec4f794406
quin <g.joaquin000@gmail.com>
2023-11-22 14:46:08 -0800

    finished adding the final touches to the ui for the submodal for the PW when creating an account, which specifies the pre-requisites in creating a password

--- frontend/src/components/layout/getStarted/modal/passwordSubModal/PasswordSubModal.js
+++ frontend/src/components/layout/getStarted/modal/passwordSubModal/PasswordSubModal.js
@@ -1,24 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './PasswordSubModal.module.css'
-
-
 const PasswordSubModal = () => {
+    const [isValidLower, setIsValidLower] = useState(false);
+    const [isValidUpper, setIsValidUpper] = useState(false);
+    const [isValidNumber, setIsValidNumber] = useState(false);
+    const [isValidSpecial, setIsValidSpecial] = useState(false);
+    const [isValidEight, setIsValidEight] = useState(false);
 
+
+
+    
     return (
         <div className={classes.subModal}>
             <div className={classes.subModalContainer}>
-                <span>At least one: </span>
-                
+                <span>Should Contain: </span>
                     <ul className={classes.row}>
-                        <li>1</li>
-                        <li>2</li>
+                        
+                        <li className={`${classes['lower']} ${isValidLower && classes.isValidLower}`}>
+                            <a className={`${classes['lowerX']} ${isValidLower && classes.isValidLower}`}>
+                                Ｘ
+                            </a>
+                            <a className={`${classes['lowerCheck']} ${isValidLower && classes.isValidLower}`}>
+                                ✓
+                            </a> 
+                            &nbsp;lowercase letter
+                        </li>
+                        <li className={`${classes['upper']} ${isValidUpper && classes.isValidUpper}`}>
+                            <a className={`${classes['upperX']} ${isValidUpper && classes.isValidUpper}`}>
+                                Ｘ
+                            </a>
+                            <a className={`${classes['upperCheck']} ${isValidUpper && classes.isValidUpper}`}>
+                                ✓
+                            </a> 
+                            &nbsp;uppercase letter
+                        </li>
                     </ul>
                     <ul className={classes.row}>
-                        <li>3</li>
-                        <li>4</li>
+                        <li className={`${classes['number']} ${isValidNumber && classes.isValidNumber}`}>
+                            <a className={`${classes['numberX']} ${isValidNumber && classes.isValidNumber}`}>
+                                Ｘ
+                            </a>
+                            <a className={`${classes['numberCheck']} ${isValidNumber && classes.isValidNumber}`}>
+                                ✓
+                            </a> 
+                            &nbsp;number
+                        </li>
+                        <li className={`${classes['special']} ${isValidSpecial && classes.isValidSpecial}`}>
+                            <a className={`${classes['specialX']} ${isValidSpecial && classes.isValidSpecial}`}>
+                                Ｘ
+                            </a>
+                            <a className={`${classes['specialCheck']} ${isValidSpecial && classes.isValidSpecial}`}>
+                                ✓
+                            </a> 
+                            &nbsp;special character
+                        </li>
                     </ul>
                     <ul className={classes.row}>
-                        <li>5</li>
+                        <li className={`${classes['eight']} ${isValidEight && classes.isValidEight}`}>
+                            <a className={`${classes['eightX']} ${isValidEight && classes.isValidEight}`}>
+                                Ｘ
+                            </a>
+                            <a className={`${classes['eightCheck']} ${isValidEight && classes.isValidEight}`}>
+                                ✓
+                            </a> 
+                            &nbsp;at least 8 characters
+                        </li>
                     </ul>
             </div>
         </div>
