f8a82b739fbc4cdf571e9cdc5511e7d9cfe75f21
quin <g.joaquin000@gmail.com>
2024-06-25 20:00:54 -0700

    still working on making the landing page more interactive

--- frontend/src/components/layout/body/1_HomeTop.js
+++ frontend/src/components/layout/body/1_HomeTop.js
@@ -7,50 +7,37 @@ import classes from './1_HomeTop.module.css';
 
 const HomeTop = () => {
 
-    return (
-        <div>
-            <div className={classes.outer_container_top}>
-                <div className={classes.inner_container_top}>
-                    <img src={BackgroundImage} alt='subjects' /> 
-                    <blockquote className={classes.caption} >
-                        <h1> LEARNING NEVER EXHAUSTS THE MIND</h1>
-                        <h3> TellusEd is adding a Montessori-flair to online-learning. Learn your way.</h3>
-                    </blockquote>
+    return (                                                                    
+        <div className={classes.outer_container_top}>                    
+            <div className={classes.inner_container_top}>     
+                <img className={classes.inner_container_img} src={BackgroundImage} alt='subjects' />         
+                <blockquote className={classes.caption} >          
+                    <h1> LEARNING NEVER EXHAUSTS THE MIND</h1>  
+                    <h3> TellusEd is adding a Montessori-flair to online-learning. Learn your way.</h3>
+                </blockquote>              
+                <div className={classes.one}>                     
+                    <img src={Logo} alt='Logo' className={classes.Logo1}></img>    
                 </div>
-                <div className={classes.splash_page_whole}>
-                    <div className={classes.splash_page_top}>
-                        <div className={classes.top_left}>
-                            <img src={Logo} alt='Logo' className={classes.Logo}></img>
-                        </div>
-                        <div className={classes.top_mid}>
-                            <img src={Logo} alt='Logo' className={classes.Logo}></img>
-                        </div>
-                        <div className={classes.top_right}>
-                            <img src={Logo} alt='Logo' className={classes.Logo}></img>
-                        </div>
-                    </div>
-                    <div className={classes.splash_page_mid}>
-                        <div className={classes.mid_left}>
-                            <img src={Logo} alt='Logo' className={classes.Logo}></img>
-                        </div>
-                        <div className={classes.mid_mid}>
-                            <img src={Logo} alt='Logo' className={classes.Logo}></img>
-                        </div>
-                        <div className={classes.mid_right}>
-                            <img src={Logo} alt='Logo' className={classes.Logo}></img>
-                        </div>
-                    </div>
-                    <div className={classes.splash_page_bot}>
-                        <div className={classes.bot_left}>
-                            <img src={Logo} alt='Logo' className={classes.Logo}></img>
-                        </div>
-                        <div className={classes.bot_mid}>
-                            <img src={Logo} alt='Logo' className={classes.Logo}></img>
-                        </div>
-                        <div className={classes.bot_right}>
-                            <img src={Logo} alt='Logo' className={classes.Logo}></img>
-                        </div>
-                    </div>
+                <div className={classes.two}>
+                    <img src={Logo} alt='Logo' className={classes.Logo2}></img>
+                </div>
+                <div className={classes.three}>
+                    <img src={Logo} alt='Logo' className={classes.Logo3}></img>
+                </div>
+                <div className={classes.four}>
+                    <img src={Logo} alt='Logo' className={classes.Logo4}></img>
+                </div>
+                <div className={classes.five}>
+                    <img src={Logo} alt='Logo' className={classes.Logo5}></img>
+                </div>
+                <div className={classes.six}>
+                    <img src={Logo} alt='Logo' className={classes.Logo6}></img>
+                </div>
+                <div className={classes.seven}>
+                    <img src={Logo} alt='Logo' className={classes.Logo7}></img>
+                </div>
+                <div className={classes.eight}>
+                    <img src={Logo} alt='Logo' className={classes.Logo8}></img>
                 </div>
             </div>
         </div>
