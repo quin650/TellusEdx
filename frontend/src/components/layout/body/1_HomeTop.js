035b71804688756b6c39e62fde9a167145693822
quin <g.joaquin000@gmail.com>
2024-06-27 13:00:35 -0700

    finished making the landing page interactive for the most part

--- frontend/src/components/layout/body/1_HomeTop.js
+++ frontend/src/components/layout/body/1_HomeTop.js
@@ -1,8 +1,6 @@
 import React from "react";
 import GetStartedButton from "../getStarted/button/GetStartedButton";
-import BackgroundImage from '../../../../static/images/Background_Limited.png';
 
-import Logo from '../../../../static/images/1Logo.png';
 import Logo1 from '../../../../static/images/Logo1.png';
 import Logo2 from '../../../../static/images/Logo2.png';
 import Logo3 from '../../../../static/images/Logo3.png';
@@ -22,14 +20,17 @@ const HomeTop = () => {
                     <h1> LEARNING NEVER EXHAUSTS THE MIND</h1>  
                     <h3> TellusEd is adding a Montessori-flair to online-learning. Learn your way.</h3>
                 </blockquote>  
-                    <img src={Logo1} alt='Logo' className={classes.Logo1}></img>               
-                    <img src={Logo2} alt='Logo' className={classes.Logo2}></img>
-                    <img src={Logo3} alt='Logo' className={classes.Logo3}></img>
-                    <img src={Logo4} alt='Logo' className={classes.Logo4}></img>
-                    <img src={Logo5} alt='Logo' className={classes.Logo5}></img>
-                    <img src={Logo6} alt='Logo' className={classes.Logo6}></img>
-                    <img src={Logo7} alt='Logo' className={classes.Logo7}></img>
-                    <img src={Logo8} alt='Logo' className={classes.Logo8}></img>
+                <img src={Logo1} alt='Logo' className={classes.Logo1} tabIndex="0"></img>               
+                <img src={Logo2} alt='Logo' className={classes.Logo2} tabIndex="0"></img>   
+                <img src={Logo3} alt='Logo' className={classes.Logo3} tabIndex="0"></img>   
+                <img src={Logo4} alt='Logo' className={classes.Logo4} tabIndex="0"></img>   
+                <img src={Logo5} alt='Logo' className={classes.Logo5} tabIndex="0"></img>   
+                <img src={Logo6} alt='Logo' className={classes.Logo6} tabIndex="0"></img>   
+                <img src={Logo7} alt='Logo' className={classes.Logo7} tabIndex="0"></img>   
+                <img src={Logo8} alt='Logo' className={classes.Logo8} tabIndex="0"></img>   
+                <div className={classes.buttonSection}>
+                    <GetStartedButton />
+                </div>
             </div>
         </div>
     );
