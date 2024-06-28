9ad20ddd8ee4d96c4b7ee3001b9dec63a9c33636
quin <g.joaquin000@gmail.com>
2024-06-28 08:59:55 -0700

    fixed a small bug with the navbar menu modal open and close button

--- frontend/src/components/layout/header/navbarMenuButton.js
+++ frontend/src/components/layout/header/navbarMenuButton.js
@@ -25,7 +25,6 @@ const NavbarMenuButton = () => {
     );
 
     let exitButton = (
-    <div className={classes.exitButtonContainer}>
         <div onClick={CloseAction} className={classes.exitButton}>
             <svg className={classes.svgExit} viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                 <path d="M6 5.293l4.789-4.79.707.708-4.79 4.79 4.79 4.789-.707.707-4.79-4.79-4.789 4.79-.707-.707L5.293 6 .502 1.211 1.21.504 6 5.294z" fillRule="nonzero" fillOpacity="1" 
@@ -33,7 +32,6 @@ const NavbarMenuButton = () => {
                 </path>
             </svg>
         </div>
-    </div>
     );
 
     const content = navbarMenuStatus ? exitButton : navbarButton;
