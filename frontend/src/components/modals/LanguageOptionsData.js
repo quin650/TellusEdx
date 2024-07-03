cc460dd341ee4268832ae32cba211fc47b56c68c
quin <g.joaquin000@gmail.com>
2024-07-02 19:47:05 -0700

    added scroll bars to both geral landing page and to the language options modal

--- frontend/src/components/modals/LanguageOptionsData.js
+++ frontend/src/components/modals/LanguageOptionsData.js
@@ -1,18 +1,88 @@
-import usaFlagImg from '../../../../frontend/static/images/countries/1UnitedStates.png';
+import USAFlagImg from '../../../../frontend/static/images/countries/1UnitedStates.png';
 import mexicoFlagImg from '../../../../frontend/static/images/countries/2Mexico.png';
+import chinaFlagImg from '../../../../frontend/static/images/countries/3China.png';
+import franceFlagImg from '../../../../frontend/static/images/countries/4France.png';
+import germanyFlagImg from '../../../../frontend/static/images/countries/5Germany.png';
+import japanFlagImg from '../../../../frontend/static/images/countries/6Japan.png';
+import portugalFlagImg from '../../../../frontend/static/images/countries/7Portugal.png';
+import russiaFlagImg from '../../../../frontend/static/images/countries/8Russia.png';
+import UAEFlagImg from '../../../../frontend/static/images/countries/9UnitedArabEmirates.png';
+import italyFlagImg from '../../../../frontend/static/images/countries/10Italy.png';
+import koreaFlagImg from '../../../../frontend/static/images/countries/11Korea.png';
+import indiaFlagImg from '../../../../frontend/static/images/countries/12India.png';
 
 const data = [
     {
         "id": 1,
         "group": 1,
         "name": "English",
-        "flag": usaFlagImg
+        "flag": USAFlagImg
     },
     {
         "id": 2,
         "group": 2,
         "name": "Spanish",
         "flag": mexicoFlagImg
+    },
+    {
+        "id": 3,
+        "group": 3,
+        "name": "China",
+        "flag": chinaFlagImg
+    },
+    {
+        "id": 4,
+        "group": 4,
+        "name": "French",
+        "flag": franceFlagImg
+    },
+    {
+        "id": 5,
+        "group": 5,
+        "name": "German",
+        "flag": germanyFlagImg
+    },
+    {
+        "id": 6,
+        "group": 6,
+        "name": "Japanese",
+        "flag": japanFlagImg
+    },
+    {
+        "id": 7,
+        "group": 7,
+        "name": "Portuguese",
+        "flag": portugalFlagImg
+    },
+    {
+        "id": 8,
+        "group": 8,
+        "name": "Russian",
+        "flag": russiaFlagImg
+    },
+    {
+        "id": 9,
+        "group": 9,
+        "name": "Arabic",
+        "flag": UAEFlagImg
+    },
+    {
+        "id": 10,
+        "group": 10,
+        "name": "Italian",
+        "flag": italyFlagImg
+    },
+    {
+        "id": 11,
+        "group": 11,
+        "name": "Korean",
+        "flag": koreaFlagImg
+    },
+    {
+        "id": 12,
+        "group": 12,
+        "name": "Hindu",
+        "flag": indiaFlagImg
     },
 ];
 
