17972c04ead6c53f8e410fb13c4d024a04f71496
quin <g.joaquin000@gmail.com>
2024-08-21 13:56:51 -0700

    added dragable bar for the questions sidebar

--- frontend/src/components/100_layout/30_sideBars/4_sideBar_R_QuestionsModal/1_sideBar_R_QuestionsModal.js
+++ frontend/src/components/100_layout/30_sideBars/4_sideBar_R_QuestionsModal/1_sideBar_R_QuestionsModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, Fragment } from "react";
+import React, { useState, useEffect, useRef, Fragment } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { userReducerActions } from "../../../../a.reducers/auth_Reducers";
 import classes from "./4_sideBar_R_QuestionsModal.module.css";
@@ -6,6 +6,7 @@ import classes from "./4_sideBar_R_QuestionsModal.module.css";
 const SideBar_R_QuestionsModal = () => {
 	const sideBarQuestionsRef = useRef();
 	const exitButtonRef = useRef();
+	const resizerRef = useRef();
 	const sideBar_R_Questions_ModalStatus_rdx = useSelector(({ user }) => user.sideBar_R_Questions_ModalStatus_rdx);
 	const dispatch = useDispatch();
 	// Exit Functionality
@@ -24,11 +25,12 @@ const SideBar_R_QuestionsModal = () => {
 			exitNavBarAction();
 		}
 	};
-	const toggleMenu = () => {
+	const exitAction = () => {
 		dispatch(userReducerActions.sideBar_R_Close_Questions_Modal());
+		sideBarQuestionsRef.current.style.width = "0px";
 	};
 	let exitButton = (
-		<button onClick={toggleMenu} className={classes.exitButton} ref={exitButtonRef}>
+		<button onClick={exitAction} className={classes.exitButton} ref={exitButtonRef}>
 			<svg className={classes.svgExit} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12">
 				<path
 					d="M6 5.293l4.789-4.79.707.708-4.79 4.79 4.79 4.789-.707.707-4.79-4.79-4.789 4.79-.707-.707L5.293 6 .502 1.211 1.21.504 6 5.294z"
@@ -40,10 +42,45 @@ const SideBar_R_QuestionsModal = () => {
 		</button>
 	);
 
+	// Resizer functionality
+	useEffect(() => {
+		const resizer = resizerRef.current;
+		const sidebar = sideBarQuestionsRef.current;
+		if (!resizer || !sidebar) return;
+		let x, w;
+		const rs_mousedownHandler = (e) => {
+			x = e.clientX;
+			// w = parseInt(window.getComputedStyle(sidebar).width, 10);
+			w = sidebar.getBoundingClientRect().width;
+			console.log("Initial sidebar width: ", w);
+			document.addEventListener("mousemove", rs_mousemoveHandler);
+			document.addEventListener("mouseup", rs_mouseupHandler);
+		};
+
+		const rs_mousemoveHandler = (e) => {
+			const dx = x - e.clientX;
+			let newWidth = w + dx; // new width
+			console.log("Resized width: ", newWidth);
+			if (newWidth < 700) {
+				sidebar.style.width = `${newWidth}px`;
+			}
+		};
+		const rs_mouseupHandler = () => {
+			document.removeEventListener("mouseup", rs_mouseupHandler);
+			document.removeEventListener("mousemove", rs_mousemoveHandler);
+		};
+		resizer.addEventListener("mousedown", rs_mousedownHandler);
+		return () => {
+			resizer.removeEventListener("mousedown", rs_mousedownHandler);
+		};
+	}, []);
+
 	return (
 		<Fragment>
 			{sideBar_R_Questions_ModalStatus_rdx ? exitButton : ""}
-			<menu className={`${classes["sideBar_R_outerContainer"]} ${sideBar_R_Questions_ModalStatus_rdx ? classes.open : ""}`} ref={sideBarQuestionsRef}></menu>
+			<menu className={`${classes["sideBar_R_outerContainer"]} ${sideBar_R_Questions_ModalStatus_rdx ? classes.open : ""}`} ref={sideBarQuestionsRef}>
+				<div className={classes.resizer} ref={resizerRef}></div>
+			</menu>
 		</Fragment>
 	);
 };
