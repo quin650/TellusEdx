06f67d684f9f539ce73d9da8dfb1276fa7cfdbef
quin <g.joaquin000@gmail.com>
2024-09-27 10:55:26 -0700

    Update DJANGO_SETTINGS_MODULE path in wsgi.py

--- backend/aa_django_project/wsgi.py
+++ backend/aa_django_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.aa_django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aa_django_project.settings')
 
 application = get_wsgi_application()
