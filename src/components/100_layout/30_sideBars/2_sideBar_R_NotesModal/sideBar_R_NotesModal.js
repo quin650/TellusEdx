04097d536e5dde5bbcffb613161431c8f3d1d744
quin <g.joaquin000@gmail.com>
2025-01-24 14:57:36 -0800

    fixed the resizer css to change color on hover

--- src/components/100_layout/30_sideBars/2_sideBar_R_NotesModal/sideBar_R_NotesModal.js
+++ src/components/100_layout/30_sideBars/2_sideBar_R_NotesModal/sideBar_R_NotesModal.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useRef, Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { userReducerActions } from "../../../../a.reducers/auth_Reducers";
-import classes from "./sideBar_R_NotesModal.module.css";
+import classes from "../../../400_demos/10_demo_DMV_ClassC/demo_DMV_ClassC.module.css";
 
-const SideBar_R_NotesModal = ({pageContentRef}) => {
+const SideBar_R_NotesModal = ({ pageContentRef }) => {
 	const sideBarRNotesRef = useRef();
 	const exitButtonRef = useRef();
 	const resizerRef = useRef();
@@ -38,81 +38,80 @@ const SideBar_R_NotesModal = ({pageContentRef}) => {
 		</button>
 	);
 
-		// Resizer functionality
-		useEffect(() => {
-			const sidebar = sideBarRNotesRef.current;
-			const pageContent = pageContentRef.current;
-			const resizer = resizerRef.current;
-			if (!resizer || !sidebar || !pageContent) return;
-	
-		
-			if (!sideBar_R_Notes_isOpen_rdx) {
-				sidebar.style.width = "0%";
-				return; 
+	// Resizer functionality
+	useEffect(() => {
+		const sidebar = sideBarRNotesRef.current;
+		const pageContent = pageContentRef.current;
+		const resizer = resizerRef.current;
+		if (!resizer || !sidebar || !pageContent) return;
+
+		if (!sideBar_R_Notes_isOpen_rdx) {
+			sidebar.style.width = "0%";
+			return;
+		}
+
+		let firstX_Width;
+		let newX_Width;
+		let sideBarRWidth;
+
+		const rs_mousedownHandler = (e) => {
+			firstX_Width = e.clientX;
+			sideBarRWidth = sidebar.getBoundingClientRect().width;
+			document.body.style.userSelect = "none";
+			document.addEventListener("mousemove", rs_mousemoveHandler);
+			document.addEventListener("mouseup", rs_mouseupHandler);
+		};
+
+		const rs_mousemoveHandler = (e) => {
+			e.preventDefault();
+			newX_Width = e.clientX;
+			let changeInX = firstX_Width - newX_Width;
+			const newSideBarWidth = sideBarRWidth + changeInX;
+
+			const viewportWidth = document.documentElement.clientWidth;
+			const sideBarLWidth = sideBar_L_isOpen_rdx ? 326 : 0;
+			const pageContentEstimatedWidth = 1500; // based on max-width (handbook_outerContainer)
+			// const pageContentCalculatedWidth = pageContent.getBoundingClientRect().width
+			const availableWidth = viewportWidth - pageContentEstimatedWidth - sideBarLWidth;
+			const minWidthPercent = 5;
+			const maxWidthPercent = 40;
+			const newWidthPercent = (newSideBarWidth / viewportWidth) * 100;
+			const availableWidthPercent = (availableWidth / availableWidth) * 100;
+
+			if (newWidthPercent > availableWidthPercent) {
+				sidebar.style.width = `${availableWidthPercent}%`;
+				stopResizing();
+				return;
 			}
-	
-			let firstX_Width;
-			let newX_Width;
-			let sideBarRWidth;
-	
-			const rs_mousedownHandler = (e) => {
-				firstX_Width = e.clientX;
-				sideBarRWidth = sidebar.getBoundingClientRect().width;
-				document.body.style.userSelect = "none";
-				document.addEventListener("mousemove", rs_mousemoveHandler);
-				document.addEventListener("mouseup", rs_mouseupHandler);
-			};
-	
-			const rs_mousemoveHandler = (e) => {
-				e.preventDefault(); 
-				newX_Width = e.clientX
-				let changeInX = firstX_Width - newX_Width;
-				const newSideBarWidth = sideBarRWidth + changeInX;
-			
-				const viewportWidth = document.documentElement.clientWidth;
-				const sideBarLWidth = sideBar_L_isOpen_rdx ? 326 : 0;
-				const pageContentEstimatedWidth = 1500  // based on max-width (handbook_outerContainer) 		
-				// const pageContentCalculatedWidth = pageContent.getBoundingClientRect().width
-				const availableWidth = viewportWidth - pageContentEstimatedWidth - sideBarLWidth;
-				const minWidthPercent = 5; 
-				const maxWidthPercent = 40; 
-				const newWidthPercent = (newSideBarWidth / viewportWidth) * 100;
-				const availableWidthPercent = (availableWidth / availableWidth) * 100
-	
-				if (newWidthPercent > availableWidthPercent) {
-					sidebar.style.width = `${availableWidthPercent}%`;
-					stopResizing();
-					return;
-				}
-			
-				if (newWidthPercent > maxWidthPercent) {
-					sidebar.style.width = `${maxWidthPercent}%`;
-					stopResizing();
-					return;
-				}
-				if (newWidthPercent < minWidthPercent) {
-					exitAction();
-					stopResizing();
-					return;
-				}
-				sidebar.style.width = `${newWidthPercent}%`;
-			};
-			
-			const stopResizing = () => {
-				document.removeEventListener("mouseup", rs_mouseupHandler);
-				document.removeEventListener("mousemove", rs_mousemoveHandler);
-				document.body.style.userSelect = "";
-			};
-			
-			const rs_mouseupHandler = () => {
-				document.removeEventListener("mouseup", rs_mouseupHandler);
-				document.removeEventListener("mousemove", rs_mousemoveHandler);
-			};
-			resizer.addEventListener("mousedown", rs_mousedownHandler);
-			return () => {
-				resizer.removeEventListener("mousedown", rs_mousedownHandler);
-			};
-		}, [sideBar_R_Notes_isOpen_rdx, sideBar_L_isOpen_rdx]);
+
+			if (newWidthPercent > maxWidthPercent) {
+				sidebar.style.width = `${maxWidthPercent}%`;
+				stopResizing();
+				return;
+			}
+			if (newWidthPercent < minWidthPercent) {
+				exitAction();
+				stopResizing();
+				return;
+			}
+			sidebar.style.width = `${newWidthPercent}%`;
+		};
+
+		const stopResizing = () => {
+			document.removeEventListener("mouseup", rs_mouseupHandler);
+			document.removeEventListener("mousemove", rs_mousemoveHandler);
+			document.body.style.userSelect = "";
+		};
+
+		const rs_mouseupHandler = () => {
+			document.removeEventListener("mouseup", rs_mouseupHandler);
+			document.removeEventListener("mousemove", rs_mousemoveHandler);
+		};
+		resizer.addEventListener("mousedown", rs_mousedownHandler);
+		return () => {
+			resizer.removeEventListener("mousedown", rs_mousedownHandler);
+		};
+	}, [sideBar_R_Notes_isOpen_rdx, sideBar_L_isOpen_rdx]);
 
 	return (
 		<Fragment>
--- src/components/100_layout/30_sideBars/4_sideBar_R_Questions/0_sideBar_R_Questions.js
+++ src/components/100_layout/30_sideBars/4_sideBar_R_Questions/0_sideBar_R_Questions.js
@@ -33,18 +33,16 @@ const SideBar_R_Questions = ({ pageContentRef }) => {
 		dispatch(userReducerActions.sideBar_R_Close_Questions());
 	};
 	let exitButton = (
-		<div className={classes.exitButtonContainer}>
-			<button onClick={exitAction} className={classes.exitButton} ref={exitButtonRef}>
-				<svg className={classes.svgExit} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12">
-					<path
-						d="M6 5.293l4.789-4.79.707.708-4.79 4.79 4.79 4.789-.707.707-4.79-4.79-4.789 4.79-.707-.707L5.293 6 .502 1.211 1.21.504 6 5.294z"
-						fillRule="nonzero"
-						fillOpacity="1"
-						stroke="none"
-					></path>
-				</svg>
-			</button>
-		</div>
+		<button onClick={exitAction} className={classes.exitButton} ref={exitButtonRef}>
+			<svg className={classes.svgExit} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12">
+				<path
+					d="M6 5.293l4.789-4.79.707.708-4.79 4.79 4.79 4.789-.707.707-4.79-4.79-4.789 4.79-.707-.707L5.293 6 .502 1.211 1.21.504 6 5.294z"
+					fillRule="nonzero"
+					fillOpacity="1"
+					stroke="none"
+				></path>
+			</svg>
+		</button>
 	);
 	// Resizer functionality
 	useEffect(() => {
@@ -142,12 +140,14 @@ const SideBar_R_Questions = ({ pageContentRef }) => {
 	}, [sideBar_R_QuestionsStatus_rdx]);
 
 	return (
-		<menu className={`${classes["sideBar_R_outerContainer"]} ${sideBar_R_Questions_isOpen_rdx ? classes.open : ""}`} ref={sideBarQuestionsRef}>
-			{exitButton}
-			<div className={classes.resizer} ref={resizerRef}></div>
+		<Fragment>
+			<menu className={`${classes["sideBar_R_outerContainer"]} ${sideBar_R_Questions_isOpen_rdx ? classes.open : ""}`} ref={sideBarQuestionsRef}>
+				{exitButton}
+				<div className={classes.resizer} ref={resizerRef}></div>
 
-			{content}
-		</menu>
+				{content}
+			</menu>
+		</Fragment>
 	);
 };
 
