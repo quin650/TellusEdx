1494b7b59390a7652cee8e7e766d58662d64542e
quin <g.joaquin000@gmail.com>
2025-02-09 20:56:51 -0800

    a lot of things, ref github

--- src/components/100_layout/30_sideBars/4_sideBar_R_Questions/6_sideBar_R_TestResultsListOfQuestions.js
+++ src/components/100_layout/30_sideBars/4_sideBar_R_Questions/6_sideBar_R_TestResultsListOfQuestions.js
@@ -6,13 +6,15 @@ import classes from "../../../400_demos/10_demo_DMV_ClassC/demo_DMV_ClassC.modul
 const SideBar_R_TestResultsListOfQuestions = ({ id, isCorrect }) => {
 	const dispatch = useDispatch();
 
+	const gotoQuestion = () => {
+		dispatch(userReducerActions.sideBar_R_Questions_GoBackTo_Test());
+		dispatch(userReducerActions.sideBar_R_Questions_GoTo_QuestionNumber(id));
+	};
+
 	return (
 		<li key={id} className={classes.contentContainer}>
 			<div className={classes.parentLabelOuterContainer}>
-				<a
-					onClick={() => dispatch(userReducerActions.setDemoCurrentPageNum(id))}
-					className={`${classes["parentLabel"]} ${isCorrect ? classes.isPassed : classes.failed}`}
-				>
+				<a onClick={gotoQuestion} className={`${classes["parentLabel"]} ${isCorrect ? classes.isPassed : classes.failed}`}>
 					<span className={classes.sectionNUm}>Q{id}:</span> {isCorrect ? "Correct" : "Incorrect"}
 				</a>
 			</div>
