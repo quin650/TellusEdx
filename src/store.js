c1bc625f4b41d5286e1ef52d0ab61e850c84f445
quin <g.joaquin000@gmail.com>
2026-01-03 12:52:06 -0800

    changed the store to not set the local vs. production environment, but now only is set from webpack which picks up variable from .env

--- src/store.js
+++ src/store.js
@@ -1,15 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userSlice from './a.reducers/auth_Reducers';
-import profSlice from './a.reducers/profile_Reducers';
-import taskSlice from './a.reducers/tasks_Reducers';
+import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "./a.reducers/auth_Reducers";
+import profSlice from "./a.reducers/profile_Reducers";
+import taskSlice from "./a.reducers/tasks_Reducers";
 
 export const store = configureStore({
 	reducer: {
 		user: userSlice.reducer,
 		prof: profSlice.reducer,
-		task: taskSlice.reducer
+		task: taskSlice.reducer,
 	},
-	devTools: process.env.NODE_ENV !== 'production',
 });
 
 export default store;
