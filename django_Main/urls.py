beba1c0f461df6b41695f334a4f96f441a2e0713
quin <g.joaquin000@gmail.com>
2026-01-18 13:23:22 -0800

    added a robots.txt url to django so that plain text is returned when robots go to tellused.com/robots.txt

--- django_Main/urls.py
+++ django_Main/urls.py
@@ -1,15 +1,35 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.http import HttpResponse
 from .views import indexView  
+
+# robots.txt is a public instruction file for search engines that lives at:
+# https://yourdomain.com/robots.txt
+# Search engines check this file before crawling your site to know:
+# What they are allowed to crawl
+# What they should avoid
+# Where your sitemap is
+# It is not for users. It is strictly for bots.
+
+def robots_txt(request):
+    content = "\n".join([
+        "User-agent: *",
+        "Disallow:",
+        "Sitemap: https://tellsed.com/sitemap.xml",
+    ])
+    return HttpResponse(content, content_type="text/plain")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', indexView),
     path('accounts/', include('django_UserAccounts.urls')),
-    path('profile/', include('user_profile.urls'))
+    path('profile/', include('user_profile.urls')),
+    path("robots.txt", robots_txt),
 ]
 
 urlpatterns += [
     re_path(r'^.*', TemplateView.as_view(template_name='index.html'))
 ]
+
+
