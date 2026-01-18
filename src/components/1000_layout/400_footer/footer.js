ad0d180eb8d0ceb5d54955adf190c7d9afa80592
quin <g.joaquin000@gmail.com>
2026-01-18 13:04:32 -0800

    changed footer to match the vervage on the image links for user using to go to home page

--- src/components/1000_layout/400_footer/footer.js
+++ src/components/1000_layout/400_footer/footer.js
@@ -20,12 +20,12 @@ const Footer = () => {
 			<div className={classes.outerContainer_Top}>
 				<ul className={classes.innerContainer_Top}>
 					<li>
-						<Link to="/" onClick={handleClickScroll} aria-label="Go to home page" className={classes.LogoContainer}>
-							<img src={Logo} alt="" className={classes.Logo} />
+						<Link to="/home" onClick={handleClickScroll} className={classes.LogoContainer}>
+							<img alt="Go To Home Page" src={Logo} className={classes.Logo} />
 						</Link>
 					</li>
 					<li>
-						<Link to="/" onClick={handleClickScroll} className={classes.companyName}>
+						<Link to="/home" onClick={handleClickScroll} className={classes.companyName}>
 							TellusEd
 						</Link>
 					</li>
